5a667b090cd2934d95bc0160c966ccce6e4b504b
abhyamgupta123 <abhyamgupta123@gmail.com>
2020-05-29 00:49:48 +0530

    Contains all the services and functions

--- main.py
+++ main.py
@@ -148,109 +148,105 @@ def searchFile(size,query):
 
 
 
-try:
-    while True:
+while True:
+    print("")
+    print("Choose a number to perform your action.")
+    print("")
+    print(" 1. To Upload a Single File.")
+    print(" 2. To download a file using it's ID to a desired local location path.")
+    print(" 3. To create a Empty Folder in Root Directory of Drive.")
+    print(" 4. To upload a file in a given Folder by it's Drive Folder ID.")
+    print(" 5. To Upload a whole folder by using it's complete path.")
+    print(" 6. To Search a File by its size and Query.")
+    print(" 7. To list all the files based on Size.")
+    print(" 8. To download folder or file by it's name.")
+    print("")
+    print("PRESS 'q' TO QUIT")
+    print("")
+    num = raw_input("Enter Option Number:  ")
+
+    if (num == "q"):
+        break
+
+    elif (int(num) == 1):
+        f_name = raw_input("Enter name of file with it's format (like .pdf, .txt, .jpg etc.): ")
+        f_path = raw_input("Enter the complete path of File: ")
+        Mime = raw_input("Enter MIME Type of file if you know otherwise just press enter: ")
+        if (Mime == ""):
+            Mime ='application/octet-stream'
         print("")
-        print("Choose a number to perform your action.")
+        uploadFile(f_name, f_path,Mime)
         print("")
-        print(" 1. To Upload a Single File.")
-        print(" 2. To download a file using it's ID to a desired local location path.")
-        print(" 3. To create a Empty Folder in Root Directory of Drive.")
-        print(" 4. To upload a file in a given Folder by it's Drive Folder ID.")
-        print(" 5. To Upload a whole folder by using it's complete path.")
-        print(" 6. To Search a File by its size and Query.")
-        print(" 7. To list all the files based on Size.")
-        print(" 8. To download folder or file by it's name.")
+        print("UPLOADING DONE..!!")
         print("")
-        print("PRESS 'q' TO QUIT")
+
+    elif (int(num) == 2):
+        f_id = raw_input("Enter file ID of File present in Google drive: ")
+        f_path = raw_input("Enter the local download path: ")
+        downloadFile(f_id, f_path)
         print("")
-        num = raw_input("Enter Option Number:  ")
+        print("DOWNLOADING DONE..!!")
+        print("")
 
-        if (num == "q"):
-            break
+    elif (int(num) == 3):
+        f_name = raw_input("Enter the name of folder: ")
+        print('')
+        createFolder(f_name)
+        print("Folder is Successfully Created.")
+        print("")
 
-        elif (int(num) == 1):
-            f_name = raw_input("Enter name of file with it's format (like .pdf, .txt, .jpg etc.): ")
-            f_path = raw_input("Enter the complete path of File: ")
-            Mime = raw_input("Enter MIME Type of file if you know otherwise just press enter: ")
-            if (Mime == ""):
-                Mime ='application/octet-stream'
+    elif (int(num) == 4):
+        d_f_id = raw_input("Enter ID of Google Drive Folder: ")
+        l_f_name = raw_input("Enter name of file with it's format (like .pdf, .txt, .jpg etc.): ")
+        l_f_path = raw_input("Enter path of local file: ")
+        l1=l_f_path.split('/')
+        print("")
+        if (l1[-1]==l_f_name):
+            upload_file_in_folder(d_f_id, l_f_name, l_file_path)
             print("")
-            uploadFile(f_name, f_path,Mime)
+            print("File created Successfully")
             print("")
-            print("UPLOADING DONE..!!")
-            print("")
-
-        elif (int(num) == 2):
-            f_id = raw_input("Enter file ID of File present in Google drive: ")
-            f_path = raw_input("Enter the local download path: ")
-            downloadFile(f_id, f_path)
-            print("")
-            print("DOWNLOADING DONE..!!")
-            print("")
-
-        elif (int(num) == 3):
-            f_name = raw_input("Enter the name of folder: ")
-            print('')
-            createFolder(f_name)
-            print("Folder is Successfully Created.")
-            print("")
-
-        elif (int(num) == 4):
-            d_f_id = raw_input("Enter ID of Google Drive Folder: ")
-            l_f_name = raw_input("Enter name of file with it's format (like .pdf, .txt, .jpg etc.): ")
-            l_f_path = raw_input("Enter path of local file: ")
-            l1=l_f_path.split('/')
-            print("")
-            if (l1[-1]==l_f_name):
-                upload_file_in_folder(d_f_id, l_f_name, l_file_path)
-                print("")
-                print("File created Successfully")
-                print("")
-            else:
-                print("Enter the correcr folder name that is present at your given path.")
-
-        elif (int(num) == 5):
-            local_f_path = raw_input("Enter path of your local folder: ")
-            local_f_name = raw_input("Enter name of your local folder: ")
-            l1=local_f_path.split('/')
-            print("")
-            if (l1[-1]==local_f_name):
-                whole_folder_upload(local_f_path, local_f_name)
-                print("")
-                print("UPLOADING DONE...!!")
-                print("")
-            else:
-                print("Enter the correcr folder name that is present at your given path.")
-
-        elif (int(num) == 6):
-            s=raw_input("Enter Size of file: ")
-            q=raw_input("Enter Query for file: ")
-            print("")
-            searchFile(s, q)
-            print("")
-
-        elif (int(num) == 7):
-            s=raw_input("Enter the Size of file: ")
-            print("")
-            listFiles(s)
-            print("")
-
-        elif (int(num)==8):
-            name= raw_input("Enter the name of folder or file present in Drive: ")
-            path=raw_input("Enter the local path where you want to download: ")
-            print("")
-            d.download(name, path)
-            print("")
-            print('DOWNLOAD DONE...!!')
-
         else:
+            print("Enter the correcr folder name that is present at your given path.")
+
+    elif (int(num) == 5):
+        local_f_path = raw_input("Enter path of your local folder: ")
+        local_f_name = raw_input("Enter name of your local folder: ")
+        l1=local_f_path.split('/')
+        print("")
+        if (l1[-1]==local_f_name):
+            whole_folder_upload(local_f_path, local_f_name)
             print("")
-            print("You entered a wrong option no. Please Enter the correct Task no. to procced further.")
+            print("UPLOADING DONE...!!")
             print("")
-except :
-    print("Something went wrong....")
-    print("Do it again carefully...")
+        else:
+            print("Enter the correcr folder name that is present at your given path.")
+
+    elif (int(num) == 6):
+        s=raw_input("Enter Size of file: ")
+        q=raw_input("Enter Query for file: ")
+        print("")
+        searchFile(s, q)
+        print("")
+
+    elif (int(num) == 7):
+        s=raw_input("Enter the Size of file: ")
+        print("")
+        listFiles(s)
+        print("")
+
+    elif (int(num)==8):
+        inp1=raw_input("enter the folder name : ")
+        inp2=raw_input("enter the location of path : ")
+        print("")
+        d.dwnld(inp1, inp2)
+        print("")
+        print('DOWNLOAD DONE...!!')
+
+    else:
+        print("")
+        print("You entered a wrong option no. Please Enter the correct Task no. to procced further.")
+        print("")
 
 
 # uploadFile('google','google.jpg','application/octet-stream')
